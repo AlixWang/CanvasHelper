29a9dccd7ade74d301fb56fb63820afffa02e3ea
wangjun17 <wangjun17@tuniu.com>
2019-03-27 13:13:39 +0800

    更改打包问价

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import common from 'rollup-plugin-commonjs'
 
 export default {
     input: 'src/index.js',
@@ -8,7 +9,11 @@ export default {
         dir: 'dist',
         format: 'umd'
     },
-    plugins: [resolve(),babel({
-        exclude: 'node_modules/**'
-    })]
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        common()
+    ]
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import {canvasHandle} from 'main.js'
-
-export default canvasHandle
+import {ObjType} from './utils/index'
+export default class canvasHandle  {
+    constructor(ctx) {
+        if(ObjType(ctx) !== 'CanvasRenderingContext2D') {
+           throw new Error('initial obj is not a canvas context objcet please check your code')
+        }
+        this.ctx = ctx
+        this.ctx.drawRoundRect = this.drawRoundRect
+    }
+    drawRoundRect() {
+        this.ctx.save()
+    }
+}
